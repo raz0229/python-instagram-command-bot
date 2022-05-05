0736678422a2d7acb2f232052fc3d2dd6d71085a
raz0229 <raja.zafar229@gmail.com>
2022-05-05 10:32:07 +0500

    added mecha

--- bot.py
+++ bot.py
@@ -7,11 +7,13 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from pynput import keyboard
+from slugify import slugify
 import pyautogui as gui
 import http.client
+import socket
+import emoji
 import json
 import gc
-
 
 options = Options()
 options.add_argument("user-data-dir=/tmp/raz0229")
@@ -20,11 +22,17 @@ PATH = "/opt/chromedriver"  # path to your downloaded webdriver
 driver = webdriver.Chrome(PATH, chrome_options=options)
 driver.get('https://instagram.com/direct/inbox')
 print(driver.title)  # prints title of the webpage
-conn = http.client.HTTPSConnection("acobot-brainshop-ai-v1.p.rapidapi.com")
+
+conn = http.client.HTTPSConnection("robomatic-ai.p.rapidapi.com", timeout=10)
 headers = {
-    'x-rapidapi-key': "85632300dbmsha01f5765f1a7303p18df83jsn83e04aa1780a",
-    'x-rapidapi-host': "acobot-brainshop-ai-v1.p.rapidapi.com"
-}
+    'content-type': "application/x-www-form-urlencoded",
+    'X-RapidAPI-Host': "robomatic-ai.p.rapidapi.com",
+    'X-RapidAPI-Key': "85632300dbmsha01f5765f1a7303p18df83jsn83e04aa1780a"
+    }
+
+
+def deemojify(text):
+    return emoji.get_emoji_regexp().sub(r'', text)
 
 
 class Bot:
@@ -39,14 +47,20 @@ class Bot:
         self.pressed_ctrl = False
 
     def make_call(self, request):
-        request = request.replace(' ', '%20').replace('\n', '')
-        conn.request("GET", f'/get?bid=178&key=sX5A2PcYZbsN5EY6&uid=mashape&msg={request}', headers=headers)
-
-        res = conn.getresponse()
-        data = res.read()
-
-        response = json.loads(data.decode('UTF-8'))["cnt"]
-        return response.replace('Aco', 'RAZ').replace('acobot.ai', 'a remote repo')  # replace name and location in response 
+        request = slugify(request, separator='%20')
+        payload = f"in={request}&op=in&cbot=1&SessionID=RapidAPI1&ChatSource=RapidAPI&cbid=1&key=RHMN5hnQ4wTYZBGCF3dfxzypt68rVP"
+        response = " "
+        try:
+            conn.request("POST", "/api.php", payload, headers)
+            res = conn.getresponse()
+            data = res.read()
+            response = json.loads(data.decode("utf-8"))["out"]
+        except socket.timeout:
+            return "ERR: Slow Internet connect on host"
+        except http.client.HTTPException:
+            return "Something went wrong. Try again"
+        else:  # no error occurred
+            return response.replace('RoboMatic', 'RAZBot').replace('robomatic.ai', 'instagram.com/raz0229')  # replace name and location in response
 
     def new_msg_received(self):
         incoming = driver.find_elements_by_css_selector('._7UhW9.xLCgt.MMzan.KV-D4.uL8Hv.T0kll')
@@ -56,10 +70,9 @@ class Bot:
             return False
 
     def send_message(self, text):
-        input_box = driver.find_elements_by_class_name('copyable-text')
-        input_box = input_box[len(input_box) - 1]
+        input_box = driver.find_element_by_css_selector('textarea')
         input_box.click()
-        input_box.send_keys(text, Keys.RETURN)
+        input_box.send_keys(deemojify(text), Keys.RETURN)
         incoming = driver.find_elements_by_css_selector('._7UhW9.xLCgt.MMzan.KV-D4.uL8Hv.T0kll')
         self.received_msgs = len(incoming)
 
@@ -81,8 +94,15 @@ class Bot:
                     self.received_msgs = len(self.incoming)
                     last_msg = self.incoming[self.received_msgs - 1].text
                     print(last_msg)
-                    self.send_message('_Thinking..._')
-                    self.send_message(self.make_call(last_msg))
+
+                    # BOT response
+                    if last_msg.lower().startswith("bot_ask"):
+                        self.send_message('Thinking...')
+
+                        if deemojify(last_msg.lower().strip()) == "bot_ask":
+                            self.send_message("Chat with me using bot_ask command. Example: bot_ask who are you")
+                        else:
+                            self.send_message(self.make_call(last_msg.replace("bot.ask", '').strip()))
                 except Exception as e:
                     summary = str(e)
                     self.send_message(summary)
